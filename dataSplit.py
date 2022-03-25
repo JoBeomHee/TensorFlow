04d6a5e3578e54766c97ea2058267b0dbebeed07
JoBeomHee <afsdzvcx123@gmail.com>
2022-03-25 21:36:29 +0900

    데이터 분리 내용

---
+++ dataSplit.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+
+
+#1 데이터
+x = np.array(range(1, 101))
+y = np.array(range(1, 101))
+
+# 6:2:2 비율로 Train:Valid:Test 셋 나눔
+x_train = x[:60]
+x_val = x[60:80]
+x_test = x[80:]
+
+y_train = y[:60]
+y_val = y[60:80]
+y_test = y[80:]
+
+# Keras의 Sequential모델을 선언
+model = keras.Sequential([
+    # 첫 번째 Layer: 데이터를 신경망에 집어넣기
+    keras.layers.Dense(32, activation='relu', input_shape = (1, )),
+    # 두번째 층 
+    keras.layers.Dense(32, activation='relu'),
+    # 세번째 출력층: 예측 값 출력하기
+    keras.layers.Dense(1)
+])
+
+# 모델을 학습시킬 최적화 방법, loss 계산 방법, 평가 방법 설정
+model.compile(optimizer='adam',loss='mse',metrics=['mse', 'binary_crossentropy'])
+
+# 모델 학습
+history = model.fit(x_train, y_train, epochs = 1000, batch_size = 1,
+                    validation_data=(x_val, y_val))
+
+# 평가 예측
+mse = model.evaluate(x_test, y_test, batch_size=1)
+print(f"mse : {mse}")
+
+y_predict = model.predict(x_test)
+print(y_test)
+
+# 결과를 그래프로 시각화
+plt.scatter(x_test, y_test, label='y_true')
+plt.scatter(x_test, model.predict(x_test), label='y_pred')
+plt.legend()
+plt.show()
